12420d54796218a4c01fdf08ac00a24ebf7182d4
Georgi Kostov <georgi.kostov@skrill.com>
2014-03-09 23:40:35 +0200

    added 2013 works

--- genlist.pl
+++ genlist.pl
@@ -4,15 +4,15 @@ while (<STDIN>) {
 	my $filename = $_;
 	chomp $filename;
 	
-	my ($title, $size, $year, $year2) = $filename =~ /^([a-z ]*) ([0-9][0-9][0-9]? x [0-9][0-9][0-9]? cm) ?([0-9][0-9][0-9][0-9])? ?([0-9][0-9][0-9][0-9])?.jpg$/gims;
+	my ($title, $size, $year, $year2) = $filename =~ /^([a-z ]*) ([0-9][0-9][0-9] cm) ?\(diagonal\)? ?([0-9][0-9][0-9][0-9])?.jpg$/gims;
 	$year = "$year-$year2" if $year2;
 	print <<"EOT";
 	<li>
-		<a class="thumb" name="$title" href="images/2012/$filename" title="$title">
-			<img src="images/2012/$filename-thumb.jpg" alt="$title" />
+		<a class="thumb" name="$title" href="images/year/$filename" title="$title">
+			<img src="images/$year/$filename-thumb.jpg" alt="$title" />
 		</a>
 		<div class="caption">
-			<div class="image-title">$title, oil/canvas: $size<br />$year</div>
+			<div class="image-title">$title, oil/canvas: $size (diagonal)<br />$year</div>
 			<div class="image-desc"></div>
 		</div>
 	</li>
