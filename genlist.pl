9b73efcce4fbce28190c866e0fae07424aa75f66
Georgi Kostov <georgi.kostov@gmail.com>
2019-05-14 22:19:31 +0300

    new paintings, about fixes and links to galleries

--- genlist.pl
+++ genlist.pl
@@ -1,19 +1,17 @@
 #!/usr/bin/perl
 
 while (<STDIN>) { 
-	my $drawing = $_;
-	chomp $drawing;
+	my $filename = $_;
+	chomp $filename;
 	
-	my ($number, $cl, $pt, $descr, $fullyear, $year) = $drawing =~ /^(\d+)\. (\w\w)\/(\w\w) (.+? mm) (.*?)(\d\d)$/gims;
-	my $filename = "$number $cl$pt$year.jpeg";
-	my $title = "$cl/$pt";
+	my ($title, $fullyear, $descr) = $filename =~ /^.*\/(.*?) (20\d\d) (oil canvas 30 x 30 cm).jpg$/gims;
 	print <<"EOT";
                     <li>
-                        <a class="thumb" name="$title" href="images/drawing/$filename" title="$title">
-                            <img class="lazyload" data-src="images/drawing/$filename" alt="$title" />
+                        <a class="thumb" name="$title" href="$filename" title="$title">
+                            <img class="lazyload" data-src="$filename" alt="$title" />
                         </a>
                         <div class="caption">
-                            <div class="image-title">$title<br/>$descr<br/>$fullyear$year</div>
+                            <div class="image-title">$title<br/>$descr<br/>$fullyear</div>
                             <div class="image-desc"></div>
                         </div>
                     </li> 
